37da5b6ee8b4d1f6d72090cfba48f4df62f14f74
info@vltmedia.com <info@vltmedia.com>
2021-05-23 17:02:23 -0400

    feature: added trainer bat for multi gpu

---
+++ RunTrain_gtavc2city_multigpu.bat
@@ -0,0 +1,1 @@
+python train.py --dataroot ./datasets/gtavc2city --name gtavc2city --model cycle_gan --batch_size 2 --save_latest_freq 400 --no_html --checkpoints_dir ./checks --save_epoch_freq 2 --gpu_ids 0,1,2,3,4

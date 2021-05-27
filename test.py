9482553c3e3a5198bec2fc76f2923bc958fda641
info@vltmedia.com <info@vltmedia.com>
2021-05-27 15:56:29 -0400

    cleaup results fixed along with sequence video

--- test.py
+++ test.py
@@ -67,13 +67,13 @@ if __name__ == '__main__':
         bypass = False
         
         if opt.only_fake == True:
-            if 'real' in img_path:
+            if '_real' in img_path:
                 bypass = True
         
-        if bypass == False:   
-            if i % 5 == 0:  # save images to an HTML file
-                print('processing (%04d)-th image... %s' % (i, img_path))
-            save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
-    
+        # if bypass == False:   
+        if i % 5 == 0:  # save images to an HTML file
+            print('processing (%04d)-th image... %s' % (i, img_path))
+        save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
+
     
     webpage.save()  # save the HTML

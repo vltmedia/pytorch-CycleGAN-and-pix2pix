c006ea88f3bd24cff7290062f27ebecd29e2baf6
info@vltmedia.com <info@vltmedia.com>
2021-05-27 10:37:33 -0400

    UnzipDataset now has args

--- unzipdatasets.sh
+++ unzipdatasets.sh
@@ -1,4 +1,5 @@
-
+import sys
 # unzip ./datasets/GTAVC-522.zip -d ./datasets/
 # unzip ./datasets/datasetimgs_prune.zip -d ./datasets/
-unzip ./datasets/gtavc2city.zip -d ./datasets/
+# unzip ./datasets/gtavc2city.zip -d ./datasets/
+unzip sys.argv[1] -d sys.argv[2]
